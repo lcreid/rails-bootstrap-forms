e2ce2d61aa35f76fb40b9d2f4707e7185e1ddd08
Larry Reid <lcreid@jadesystems.ca>
2019-06-02 10:22:56 -0700

    Closed #523 with documentation

--- demo/app/controllers/bootstrap_controller.rb
+++ demo/app/controllers/bootstrap_controller.rb
@@ -11,4 +11,21 @@ class BootstrapController < ApplicationController
     @user_with_error.errors.add(:email)
     @user_with_error.errors.add(:misc)
   end
+
+  def static_control
+    @collection = [
+      Address.new(id: 1, street: "Foo"),
+      Address.new(id: 2, street: "Bar")
+    ]
+
+    @user = User.new
+
+    @user_with_error = User.new
+    @user_with_error.errors.add(:email)
+    @user_with_error.errors.add(:misc)
+  end
+
+  def create
+    redirect_to "/bootstrap/static_control"
+  end
 end
--- demo/config/routes.rb
+++ demo/config/routes.rb
@@ -2,4 +2,7 @@ Dummy::Application.routes.draw do
   resources :users
 
   root to: "bootstrap#form"
+  get "/bootstrap/static_control", to: "bootstrap#static_control"
+  # post "admin/users", to: "bootstrap#create", as: "admin_users_path"
+  post "/admin/users", to: "bootstrap#create", as: "admin_users"
 end
